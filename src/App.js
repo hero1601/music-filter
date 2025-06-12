b6e3b70662a2bea6e5f9bf83b702d074b27643ba
hero1601 <amadhusudan1601@gmail.com>
2025-06-11 19:44:03 -0700

    update frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import config from './config';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+
+  const search = async () => {
+    const res = await axios.get(`${config.BACKEND_URL}/search`, {
+      params: { query }
+    });
+    setResults(res.data.results);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lyrics Search</h1>
+      <input value={query} onChange={e => setQuery(e.target.value)} />
+      <button onClick={search}>Search</button>
+      <div>
+        {results.map(song => (
+          <div key={song.id}>
+            <h3>{song.title} - {song.artist}</h3>
+            <ul>
+              {song.matched_lines.map((line, i) => (
+                <li key={i} dangerouslySetInnerHTML={{ __html: line.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>") }} />
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
